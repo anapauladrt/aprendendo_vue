cb37668cb4504531772e701a25467a09a0b5f1a5
anapauladrt <apdluz.l@gmail.com>
2026-02-17 17:26:57 -0300

    class 2

---
+++ vue-estagio/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+
+const numero = ref(0) 
+
+function aumentar() {
+  numero.value++
+}
+</script>
+
+<template>
+  <h1>{{ numero }}</h1>
+  <button @click="aumentar">+1</button>
+</template>
