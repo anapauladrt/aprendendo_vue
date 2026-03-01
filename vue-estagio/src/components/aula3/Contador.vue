dfc179f6c79fa18350a3e94048129d898b8fe4f6
anapauladrt <apdluz.l@gmail.com>
2026-03-01 16:47:50 -0300

    class 4

---
+++ vue-estagio/src/components/aula3/Contador.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+
+const numero = ref(0) 
+const nome = ref('')
+
+function aumentar() {
+  numero.value++
+  console.log('teste')
+}
+function subtrair() {
+  numero.value--
+}
+</script>
+
+<template>
+  <input v-model="nome" placeholder="Digite seu nome"/>
+  <p>Olá {{ nome }}!</p>
+  <h1>{{ numero }}</h1>
+  <button @click="aumentar">+1</button>
+  <button @click="subtrair">-1</button>
+  <p v-if="numero < 0">negativo</p>
+  <p v-else-if="numero == 0">zero</p>
+  <p v-else>positivo</p>
+</template>
